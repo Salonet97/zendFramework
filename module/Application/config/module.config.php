671855aac4d20c2bfdb25faddfd9ca2d54b55466
Salonet97 <salo666n@gmail.com>
2020-04-04 22:51:29 -0600

    Add files via upload

---
+++ module/Application/config/module.config.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Application;
+
+use Zend\Router\Http\Literal;
+use Zend\Router\Http\Segment;
+use Zend\ServiceManager\Factory\InvokableFactory;
+
+return [
+    'router' => [
+        'routes' => [
+			'actividad' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route'    => '/actividad[/:action]',
+                    'defaults' => [
+                        'controller' => Controller\ActividadController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+			'ponente' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route'    => '/ponente[/:action]',
+                    'defaults' => [
+                        'controller' => Controller\PonenteController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+			'organizador' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route'    => '/organizador[/:action]',
+                    'defaults' => [
+                        'controller' => Controller\OrganizadorController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+            'categoria' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route'    => '/categoria[/:action]',
+                    'defaults' => [
+                        'controller' => Controller\CategoriaController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+            'participante' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route'    => '/participante[/:action]',
+                    'defaults' => [
+                        'controller' => Controller\ParticipanteController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+            'home' => [
+                'type'    => Literal::class,
+                'options' => [
+                    'route'    => '/',
+                    'defaults' => [
+                        'controller' => Controller\IndexController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+            'application' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route'    => '/application[/:action]',
+                    'defaults' => [
+                        'controller' => Controller\IndexController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+			
+        ],
+    ],
+    'controllers' => [
+        'factories' => [
+            Controller\IndexController::class => InvokableFactory::class,
+			Controller\PonenteController::class => InvokableFactory::class,
+			Controller\OrganizadorController::class => InvokableFactory::class,
+			Controller\ActividadController::class => InvokableFactory::class,
+			Controller\CategoriaController::class => InvokableFactory::class,
+            Controller\ParticipanteController::class => InvokableFactory::class,
+        ],
+    ],
+    'view_manager' => [
+        'display_not_found_reason' => true,
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
+        'template_map' => [
+            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
+            'error/404'               => __DIR__ . '/../view/error/404.phtml',
+            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+        ],
+        'template_path_stack' => [
+            __DIR__ . '/../view',
+        ],
+    ],
+];
---
+++ module/Application/src/Controller/PonenteController.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class PonenteController extends AbstractActionController {
+
+	public function indexAction() {
+		return new ViewModel();
+	}
+
+	public function editAction() {
+		return new ViewModel();
+	}
+
+	public function createAction() {
+		return new ViewModel();
+	}
+
+}
